60ac65e8b911b32cc7fb5af5da8acde978e6912c
datdao5898 <datdao5898@gmail.com>
2026-07-04 15:31:40 +0700

    Configure Apps Script wishes endpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Dán URL Web App của Google Apps Script vào đây sau khi deploy.
 // Ví dụ: window.WEDDING_WISHES_APP_SCRIPT_URL = "https://script.google.com/macros/s/xxxxx/exec";
-window.WEDDING_WISHES_APP_SCRIPT_URL = "";
+window.WEDDING_WISHES_APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxmu8JrMy8XebLRshebw0LDA3vThuBzscvl9oHOmzOJ-jFfeYFRFj5yt6SLXI5Hm-s2zg/exec";
