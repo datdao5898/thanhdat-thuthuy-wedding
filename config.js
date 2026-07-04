d630e9e3f700a07b849f4af3b94c7048d9840bc7
datdao5898 <datdao5898@gmail.com>
2026-07-04 15:55:21 +0700

    Update Apps Script RSVP endpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Dán URL Web App của Google Apps Script vào đây sau khi deploy.
 // Ví dụ: window.WEDDING_WISHES_APP_SCRIPT_URL = "https://script.google.com/macros/s/xxxxx/exec";
-window.WEDDING_WISHES_APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwJWHGZmaz2LlRE5A7moGPHzIPxrsiDZZXVH7tZGNxIc64iWREyGp3NobrnG0J8OM9iOQ/exec";
+window.WEDDING_WISHES_APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzpv2YHi9rNTool2xK7mEK3M5EhCTM_gziioKMadlOpAWpyehxRD6xVeP6T6JM_n74Muw/exec";
